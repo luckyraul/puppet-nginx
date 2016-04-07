2c26ace1e9b87e09b919397501bb9a88bc8443e5
Nikita Tarasov <nikita@mygento.ru>
2016-04-07 13:20:12 +0300

    service fix

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,11 +1,11 @@
 # == Class: nginx::service
-class nginx::service {
+class nginx::service (
     $service_ensure   = $nginx::service_ensure,
     $service_enable   = $nginx::service_enable,
     $service_restart  = $nginx::service_restart,
-}
+)
 {
-    service {'nginx' :
+    service {'nginx':
         ensure     => $service_ensure,
         enable     => $service_enable,
         hasstatus  => true,
