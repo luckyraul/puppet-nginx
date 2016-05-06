f9377e522f43cb83c85c72dd3bc4c2ee4e21fc03
Nikita Tarasov <nikita@mygento.ru>
2016-05-07 00:07:40 +0300

    require

--- manifests/init.pp
+++ manifests/init.pp
@@ -55,7 +55,7 @@ class nginx (
 
     ### HIERA ###
     $nginx_vhosts                  = {},
-    $nginx_vhosts_defaults         = {},
+    $nginx_vhosts_defaults         = {require => Class['nginx::service']},
 
     ) inherits nginx::params
 {
