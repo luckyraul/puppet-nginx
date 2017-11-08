2c141a3234b555fd03df03111f2610d66083c71c
Nikita Tarasov <nikita@mygento.ru>
2017-11-08 22:04:34 +0300

    extra_cfg on top

--- manifests/resources/vhost.pp
+++ manifests/resources/vhost.pp
@@ -77,6 +77,7 @@ define nginx::resources::vhost (
     $type                   = undef,
     $charset                = undef,
     $autoindex              = undef,
+    $extra_cfg              = undef,
 
     $external               = false,
 )
